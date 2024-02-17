6ccbae5921edef01d0554e8b7e22a86aa30fd9d9
= <=>
2024-02-17 14:53:25 -0300

    Atualizações Heroku - 17021453

---
+++ dist/routes.js
@@ -0,0 +1,30 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+const express_1 = require("express");
+const multer_1 = __importDefault(require("multer"));
+const CreateUserController_1 = require("./controllers/user/CreateUserController");
+const AuthUserController_1 = require("./controllers/user/AuthUserController");
+const DetailUserController_1 = require("./controllers/user/DetailUserController");
+const CreateSchedulingController_1 = require("./controllers/scheduling/CreateSchedulingController");
+const RemoveSchedulingController_1 = require("./controllers/scheduling/RemoveSchedulingController");
+const ListSchedulingsController_1 = require("./controllers/scheduling/ListSchedulingsController");
+const FinishSchedulingController_1 = require("./controllers/scheduling/FinishSchedulingController");
+const isAuthenticated_1 = require("./middlewares/isAuthenticated");
+const multer_2 = __importDefault(require("./config/multer"));
+const router = (0, express_1.Router)();
+exports.router = router;
+//pasta onde será gravado as imagens
+const upload = (0, multer_1.default)(multer_2.default.upload("./tmp"));
+//-- ROTAS USER --
+router.post('/users', new CreateUserController_1.CreateUserController().handle);
+router.post('/session', new AuthUserController_1.AuthUserController().handle);
+router.get('/me', isAuthenticated_1.isAuthenticated, new DetailUserController_1.DetailuserController().handle);
+//-- ROTAS SCHEDULING --
+router.post('/Scheduling', isAuthenticated_1.isAuthenticated, new CreateSchedulingController_1.CreateSchedulingController().handle);
+router.delete('/Scheduling', isAuthenticated_1.isAuthenticated, new RemoveSchedulingController_1.RemoveSchedulingController().handle);
+router.get('/Schedulings', isAuthenticated_1.isAuthenticated, new ListSchedulingsController_1.ListSchedulingsController().handle);
+router.put('/Scheduling/finish', isAuthenticated_1.isAuthenticated, new FinishSchedulingController_1.FinishSchedulingController().handle);
